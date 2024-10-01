c51a470d6f30346766d75ff60b1884234eef455b
leventbesli <levent.besli@gmail.com>
2024-10-01 11:17:29 +0300

    Update setup_btrfs_grub.sh
    - timeline service enabled by default
    - custom timeline limits added
    - grub update function handed over to timeline service

--- setup_btrfs_grub.sh
+++ setup_btrfs_grub.sh
@@ -11,9 +11,9 @@ printf "\033[1;33mStarting the setup process...\033[0m\n"
 
 # Install Btrfs-Assistant/snapper
 printf "\033[1;33mInstalling btrfs-assistant/snapper and dependencies...\033[0m\n"
-dnf install -y btrfs-assistant make inotify-tools python3-dnf-plugin-snapper
+dnf install -y btrfs-assistant make inotify-tools python3-dnf-plugin-snapper git
 
-# Create first snapper root config and create a manual snapshot
+# Create first snapper root config and create a new manual snapshot
 printf "\033[1;33mCreating snapper root config and taking the first root snapshot...\033[0m\n"
 snapper -c root create-config / && snapper -c root create --description "Manual Snapshot"
 
@@ -22,7 +22,7 @@ printf "\033[1;33mInstalling grub-btrfs...\033[0m\n"
 git clone https://github.com/Antynea/grub-btrfs
 cd grub-btrfs
 
-# Modify grub-btrfs config for Fedora
+# Modify grub-btrfs config for Fedora before installing
 printf "\033[1;33mModifying grub-btrfs configuration for Fedora...\033[0m\n"
 sed -i '/#GRUB_BTRFS_SNAPSHOT_KERNEL/a GRUB_BTRFS_SNAPSHOT_KERNEL_PARAMETERS="systemd.volatile=state"' config
 sed -i '/#GRUB_BTRFS_GRUB_DIRNAME/a GRUB_BTRFS_GRUB_DIRNAME="/boot/grub2"' config
@@ -31,7 +31,13 @@ sed -i '/#GRUB_BTRFS_SCRIPT_CHECK=/a GRUB_BTRFS_SCRIPT_CHECK=grub2-script-check'
 
 # Install grub-btrfs
 printf "\033[1;33mInstalling grub-btrfs...\033[0m\n"
-sudo make install
+make install
+
+# Updating timeline limits
+printf "\033[1;33mUpdating timeline limits...\033[0m\n"
+sed -i 's/^TIMELINE_LIMIT_HOURLY="[^"]*"/TIMELINE_LIMIT_HOURLY="1"/' /etc/snapper/configs/root
+sed -i 's/^TIMELINE_LIMIT_DAILY="[^"]*"/TIMELINE_LIMIT_DAILY="2"/' /etc/snapper/configs/root
+sed -i 's/^TIMELINE_LIMIT_WEEKLY="[^"]*"/TIMELINE_LIMIT_WEEKLY="1"/' /etc/snapper/configs/root
 
 # Update grub.cfg and enable grub-btrfsd service
 printf "\033[1;33mUpdating grub configuration and enabling grub-btrfsd service...\033[0m\n"
@@ -42,28 +48,19 @@ printf "\033[1;33mCleaning up installation files...\033[0m\n"
 cd ..
 rm -rf grub-btrfs
 
-# Set up automatic Grub update after snapshot creation
-printf "\033[1;33mSetting up automatic grub update after snapshots...\033[0m\n"
-mkdir -p /etc/snapper/hooks
+# Starting snapper-timeline.timer service
+printf "\033[1;33mStarting snapper-timeline.timer service...\033[0m\n"
+systemctl start snapper-timeline.timer && systemctl enable snapper-timeline.timer
 
-# Create update-grub.sh script
-cat <<EOL > /etc/snapper/hooks/update-grub.sh
-#!/bin/bash
-if [[ "\$1" == "post" ]]; then
-  echo "Running grub-mkconfig due to snapshot creation"
-  sudo grub-mkconfig -o /boot/grub2/grub.cfg
-fi
-EOL
-
-chmod +x /etc/snapper/hooks/update-grub.sh
-
-# Add hooks to snapper config
-printf "\033[1;33mAdding snapper hook configuration...\033[0m\n"
-echo 'HOOKS="/etc/snapper/hooks/update-grub.sh"' >> /etc/snapper/configs/root
+# Starting snapper-cleanup.timer service
+printf "\033[1;33mStarting snapper-timeline.cleanup service...\033[0m\n"
+systemctl start snapper-cleanup.timer && systemctl enable snapper-cleanup.timer
 
 # Restart grub-btrfsd service
 printf "\033[1;33mRestarting grub-btrfsd service...\033[0m\n"
-sudo systemctl restart grub-btrfsd
+systemctl restart grub-btrfsd && systemctl enable grub-btrfsd
+
+
 
 printf "\033[1;33mSetup complete. Grub-btrfs and automatic snapshot configuration is now active.\033[0m\n"
 printf "\033[1;33mHint: You can manage snapshots from the GUI with Btrfs Assistant.\033[0m\n"
